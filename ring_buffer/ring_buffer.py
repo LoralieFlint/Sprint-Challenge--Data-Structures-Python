cc81f0f1c9df164af8a34356fd621dd60ae8662f
LoralieFlint <loralie.flint0128@gmail.com>
2020-09-25 18:47:22 -0700

    ring buffer complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,30 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.buffer = list()
+        self.capicity_tracker = capacity
 
+ 
     def append(self, item):
-        pass
+        if self.capacity > 0:
+            self.buffer.append(item)
+            self.capacity -= 1
+        
+        elif self.capacity == 0:
+            self.capacity = self.capicity_tracker * -1
+            self.buffer[self.capacity] = item
+            self.capacity += 1
+
+        elif self.capacity < 0:
+            self.buffer[self.capacity] = item
+            self.capacity += 1
+
 
     def get(self):
-        pass
+      return self.buffer
+
+buffer = RingBuffer(3)
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+print(buffer)
